fda7a45fb838f306b8fe9d923fba1ba9a76cbad8
aeliseev <a.s.eliseev@bk.ru>
2022-01-24 21:52:01 +0400

    start project

---
+++ src/main/java/ws/eliseev/fitness/FitnessApplication.java
@@ -0,0 +1,12 @@
+package ws.eliseev.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessApplication.class, args);
+    }
+}
