83dd29c08507372fc1f0fa1104037f8718f9cb48
dmitryprikhunov <dp2149@gmail.com>
2022-01-29 09:48:01 +0400

    - Added OpenAPI Swagger v3 dependencies and SpringDoc OpenAPI maven plugin - Some useful links in FitnessApplication.java and basic @OpenApiDefinition

--- src/main/java/ws/eliseev/fitness/FitnessApplication.java
+++ src/main/java/ws/eliseev/fitness/FitnessApplication.java
@@ -1,11 +1,16 @@
 package ws.eliseev.fitness;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// Use http://localhost:8082/swagger-ui/index.html to open UI
+// or http://localhost:8082/v3/api-docs.yaml to generate YAML file
+// you can also use http://localhost:8082/v3/api-docs without .yaml to open docs in browser
+@OpenAPIDefinition(info = @Info(title = "FITNESS API", version = "IN DEV", description = "Fitness App"))
 public class FitnessApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FitnessApplication.class, args);
     }
