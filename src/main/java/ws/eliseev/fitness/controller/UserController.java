d12293079db980f41044782e1aaaf0a40cab4c89
Artem41 <zykov1979@bk.ru>
2022-02-02 14:16:32 +0300

    Fit-14 Rest контроллеры для пользователей. Реализован слой Repository. Реализован слой service.

--- src/main/java/ws/eliseev/fitness/controller/UserController.java
+++ src/main/java/ws/eliseev/fitness/controller/UserController.java
@@ -11,87 +11,47 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class UserController {
-
 
     private final IUserService iUserService;
 
-
-    /**
-     * Возвращаем список всех User-ов из базы данных
-     * @return
-     */
-    @GetMapping("/users")
+    @GetMapping()
     public ResponseEntity<List<User>> showAllUsers() {
         return new ResponseEntity<>(iUserService.getAllUser(), HttpStatus.OK);
     }
 
-    /**
-     * Добавляем в базу данных нового User-а
-     * @param user
-     * @return
-     */
-    @PostMapping("/users")
+    @PostMapping()
     public ResponseEntity<User> addNewUser(@RequestBody User user) {
         return new ResponseEntity<>(iUserService.saveUser(user), HttpStatus.OK);
     }
 
-    /**
-     * Изменяем данные User-а в базу данных
-     * @param user
-     * @return
-     */
-    @PutMapping("/users")
+    @PutMapping()
     public ResponseEntity<User> updateUser(@RequestBody User user) {
         return new ResponseEntity<>(iUserService.saveUser(user), HttpStatus.OK);
     }
 
-    /**
-     * Получаем User-а по id из базы данных
-     * @param id
-     * @return
-     */
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         return new ResponseEntity<>(iUserService.getUserById(id), HttpStatus.OK);
     }
 
-    /**
-     * Получаем User-а по его username из базы данных
-     * @param username
-     * @return
-     */
-    @GetMapping("/users/{username}")
+    @GetMapping("/{username}")
     public ResponseEntity<User> getUserByUseName(@PathVariable String username) {
         return new ResponseEntity<>(iUserService.getByUserName(username), HttpStatus.OK);
     }
 
-    /**
-     * Получаем User-а по его email из базы данных
-     * @param email
-     * @return
-     */
-    @GetMapping("/users/{email}")
+    @GetMapping("/{email}")
     public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
         return new ResponseEntity<>(iUserService.getUserByEmail(email), HttpStatus.OK);
     }
 
-    /**
-     * Получаем User-а по номеру его телефона из базы данных
-     * @param phone
-     * @return
-     */
-    @GetMapping("/users/{phone}")
+    @GetMapping("/{phone}")
     public ResponseEntity<User> getUserByPhone(@PathVariable String phone) {
         return new ResponseEntity<>(iUserService.getUserByPhone(phone), HttpStatus.OK);
     }
 
-    /**
-     * Удаляем User-а из базы данных
-     * @param id
-     */
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/{id}")
     public void deleteUserById(@PathVariable Long id) {
         iUserService.deleteUserById(id);
     }
--- src/main/java/ws/eliseev/fitness/service/IUserService.java
+++ src/main/java/ws/eliseev/fitness/service/IUserService.java
@@ -5,18 +5,57 @@ import ws.eliseev.fitness.model.User;
 import java.util.List;
 
 public interface IUserService {
-
+    /**
+     * Возвращаем список всех User-ов из базы данных
+     *
+     * @return
+     */
     List<User> getAllUser();
 
+    /**
+     * Добавляем/изменяем в базе данных нового User-а
+     *
+     * @param user
+     * @return
+     */
     User saveUser(User user);
 
+    /**
+     * Получаем User-а по id из базы данных
+     *
+     * @param id
+     * @return
+     */
     User getUserById(Long id);
 
+    /**
+     * Получаем User-а по его username из базы данных
+     *
+     * @param username
+     * @return
+     */
     User getByUserName(String username);
 
+    /**
+     * Получаем User-а по его email из базы данных
+     *
+     * @param email
+     * @return
+     */
     User getUserByEmail(String email);
 
+    /**
+     * Получаем User-а по номеру его телефона из базы данных
+     *
+     * @param phone
+     * @return
+     */
     User getUserByPhone(String phone);
 
+    /**
+     * Удаляем User-а из базы данных
+     *
+     * @param id
+     */
     void deleteUserById(Long id);
 }
