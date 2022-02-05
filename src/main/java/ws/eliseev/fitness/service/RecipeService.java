ac214e0f63aa4405603ecd2c8a2bfb097859f014
Dmitry Prikhunov <dp2149@gmail.com>
2022-02-05 16:17:28 +0400

    FIT-16 (#3)
    * - Added Recipe Controller, Recipe Service, Recipe Repository, Recipe Entity by Pavel Khramov
    - Swagger integration for APIs (some useful annotations and comments)
    
    * - Исправлены методы на ResponseEntity
    - Добавлены JavaDoc
    - Переименованы классы и интерфейсы
    - Убраны ненужные IF
    
    * - Добавлены недостающие Javadoc
    - Переведены некоторые комментарии
    - @RequestMapping в контроллере (убран повторяющийся код)
    - Убран xml для бд
    
    * - Удален Recipe
    
    * - Добавлены комментарии в IRecipeService

---
+++ src/main/java/ws/eliseev/fitness/service/RecipeService.java
@@ -0,0 +1,42 @@
+package ws.eliseev.fitness.service;
+
+import org.springframework.stereotype.Service;
+import ws.eliseev.fitness.repository.IRecipeRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RecipeService implements IRecipeService {
+
+    private final IRecipeRepository recipeRepository;
+
+    public RecipeService(IRecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public List<Recipe> fetchRecipeList() {
+        return recipeRepository.findAll();
+    }
+
+    @Override
+    public Recipe saveOrUpdateRecipe(Recipe recipe) {
+        return recipeRepository.save(recipe);
+    }
+
+    @Override
+    public Optional<Recipe> fetchRecipeById(Long recipeId) {
+        return recipeRepository.findById(recipeId);
+    }
+
+    @Override
+    public List<Recipe> fetchRecipeByName(String name) {
+        return recipeRepository.findAllByName(name);
+    }
+
+    @Override
+    public void deleteRecipeById(Long recipeId) {
+        recipeRepository.deleteById(recipeId);
+    }
+}
