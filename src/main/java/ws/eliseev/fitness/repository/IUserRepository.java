55fb356f2849035d15b30a6f418a3b5d9a35ffdd
Valentin <98877649+ValentPr@users.noreply.github.com>
2022-02-10 13:48:17 +0300

    Delete IUserRepository.java
    удалил IUserRepository.java

--- src/main/java/ws/eliseev/fitness/repository/IUserRepository.java
+++
@@ -1,7 +0,0 @@
-package ws.eliseev.fitness.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ws.eliseev.fitness.model.User;
-
-public interface IUserRepository extends JpaRepository<User, Long> {
-}
