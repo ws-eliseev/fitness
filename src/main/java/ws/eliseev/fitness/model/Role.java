756e6a7963ea30e896a72db6a1537ecb04414e13
Роман <Roks420@yandex.ru>
2022-02-12 03:21:30 +0300

    FIT-28 неокончательный вариант инициализации БД ролями и пользователями. Также были добавлены комментарии и конструкторы в классы Role.java и User.java

--- src/main/java/ws/eliseev/fitness/model/Role.java
+++ src/main/java/ws/eliseev/fitness/model/Role.java
@@ -8,20 +8,35 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+ * Класс для хранения информации о роли (правах) пользователе в программе и в БД
+ * @author Кончалов Роман
+ */
+
 @Entity
 @Table(name = "FIT_ROLE")
 @Getter
 @Setter
 @NoArgsConstructor
 public class Role {
+
+    /* Первичный ключ с генерацией значения по возрастанию, 8 байт */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column (name = "ID")
     private Long id;
 
+    /* Наименование роли с префиксом "ROLE_", не более 255 символов */
     @Column (name = "NAME")
     private String name;
 
+    /* Конструктор для удобного заполнения тестовыми данными */
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /* Коллекция пользователей обладающих данной ролью для реализации связи "многие ко многим" */
     @JsonIgnore
     @Audited
     @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
