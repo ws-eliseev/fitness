9dbfd019833233876392a31b467006f24fa30a4a
Melnik Andrey <melnik_blr1987@mail.ru>
2022-03-17 16:35:13 +0300

    Добавил в recipe поля и изменил структуру таблиц

---
+++ src/main/java/ws/eliseev/fitness/model/Ingredient.java
@@ -0,0 +1,49 @@
+package ws.eliseev.fitness.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "FIT_INGREDIENT")
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @ManyToMany(mappedBy = "ingredients")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Recipe> getRecipe() {
+        return recipes;
+    }
+
+    public void setRecipe(Set<Recipe> recipe) {
+        this.recipes = recipe;
+    }
+}
--- src/main/java/ws/eliseev/fitness/model/Recipe.java
+++ src/main/java/ws/eliseev/fitness/model/Recipe.java
@@ -13,39 +13,61 @@ import java.util.*;
 @AllArgsConstructor
 public class Recipe {
 
+    /** Поле id - Первичный ключ, генерация IDENTITY*/
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private Long id;
 
+
+    /** Поле наименования рецепта */
     @Column(name = "NAME", nullable = false)
     private String name;
 
+
+    /** Поле имени уникального  рецепта */
     @Column(name = "UNIQUE_RECIPE_FIELD")
     private String uniqueRecipeField;
 
+    /** Поле количества калорий */
     @Column(name = "CALORIES")
     private int calories;
 
+    /** Поле количества белков */
     @Column(name = "PROTEINS")
     private int proteins;
 
+    /** Поле количества жиров */
     @Column(name = "FAT")
     private int fat;
 
+    /** Поле количества углеводов */
     @Column(name = "CARBOHYDRATES")
     private int carbohydrates;
+
+    /** Поле для указания ингредиентов */
+    @ManyToMany
+    @JoinTable(
+            name = "FIT_RECIPE_INGREDIENT"
+            , joinColumns = @JoinColumn(name = "RECIPE_ID")
+            , inverseJoinColumns = @JoinColumn(name = "INGREDIENT_ID"))
+    private Set <Ingredient> ingredients = new HashSet<>() ;
+
+
+    /** Поле изображения рецепта */
+    @Column(name = "IMAGE")
+    private String image;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Recipe recipe = (Recipe) o;
-        return calories == recipe.calories && proteins == recipe.proteins && fat == recipe.fat && carbohydrates == recipe.carbohydrates && Objects.equals(name, recipe.name) && Objects.equals(uniqueRecipeField, recipe.uniqueRecipeField);
+        return calories == recipe.calories && proteins == recipe.proteins && fat == recipe.fat && carbohydrates == recipe.carbohydrates && Objects.equals(id, recipe.id) && Objects.equals(name, recipe.name) && Objects.equals(uniqueRecipeField, recipe.uniqueRecipeField) && Objects.equals(ingredients, recipe.ingredients) && Objects.equals(image, recipe.image);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, uniqueRecipeField, calories, proteins, fat, carbohydrates);
+        return Objects.hash(id, name, uniqueRecipeField, calories, proteins, fat, carbohydrates, ingredients, image);
     }
 }
