ab385f64afe72b4994571afb05e702858250bb9c
Constantin Vashchuk <iberia@bk.ru>
2022-03-30 12:01:36 +0300

    * добавлены зависимости hibernate-orm-search, hibernate-entitymanager, hibernate-envers

---
+++ src/main/java/ws/eliseev/fitness/service/search/RecipeSearch.java
@@ -0,0 +1,50 @@
+package ws.eliseev.fitness.service.search;
+
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.query.dsl.QueryBuilder;
+import org.springframework.stereotype.Repository;
+import ws.eliseev.fitness.model.Recipe;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public class RecipeSearch {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Recipe> search(String q) {
+        // get the full text entity manager
+        FullTextEntityManager fullTextEntityManager =
+                org.hibernate.search.jpa.Search.
+                        getFullTextEntityManager(entityManager);
+
+        // create the query using Hibernate Search query DSL
+        QueryBuilder queryBuilder =
+                fullTextEntityManager.getSearchFactory()
+                        .buildQueryBuilder().forEntity(Recipe.class).get();
+
+        // a very basic query by keywords
+        org.apache.lucene.search.Query query =
+                queryBuilder
+                        .keyword()
+                        .onFields("name",
+                                "uniqueRecipeField",
+                                "meals")
+                        .matching(q)
+                        .createQuery();
+
+        // wrap Lucene query in an Hibernate Query object
+        org.hibernate.search.jpa.FullTextQuery jpaQuery =
+                fullTextEntityManager.createFullTextQuery(query, Recipe.class);
+
+        // execute search and return results (sorted by relevance as default)
+        @SuppressWarnings("unchecked")
+        List results = jpaQuery.getResultList();
+
+        return results;
+    }
+}
