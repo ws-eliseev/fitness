3b8a4099e602e11b8cab31f0118f5436355eae1b
Constantin Vashchuk <iberia@bk.ru>
2022-04-01 12:20:36 +0300

    * изменен SecurityConfig

--- src/main/java/ws/eliseev/fitness/config/SecurityConfig.java
+++ src/main/java/ws/eliseev/fitness/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package ws.eliseev.fitness.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,7 +13,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ws.eliseev.fitness.security.jwt.JwtAuthenticationEntryPoint;
 import ws.eliseev.fitness.security.jwt.JwtFilter;
+import ws.eliseev.fitness.service.UserDetailsServiceImpl;
 
 /**
  * @author Costa Vashchuk
@@ -22,24 +27,59 @@ import ws.eliseev.fitness.security.jwt.JwtFilter;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtFilter jwtFilter;
+    private  final UserDetailsServiceImpl userDetailsService;
+    private final JwtAuthenticationEntryPoint unAuthorizeHandler;
 
-    public SecurityConfig(JwtFilter jwtFilter) {
+    @Autowired
+    public SecurityConfig(JwtFilter jwtFilter,
+                          UserDetailsServiceImpl userDetailsService,
+                          JwtAuthenticationEntryPoint unAuthorizeHandler) {
         this.jwtFilter = jwtFilter;
+        this.userDetailsService = userDetailsService;
+        this.unAuthorizeHandler = unAuthorizeHandler;
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic().disable()
-                .csrf().disable()
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(unAuthorizeHandler)
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/auth/login", "/api/auth/token", "/api/auth/refresh").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                .antMatchers("/",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js")
+                .permitAll()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        // Add our custom JWT security filter
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
     }
 
     @Bean
